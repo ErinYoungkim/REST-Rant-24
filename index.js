cad81c66280cd037b807c35345781c1409992102
Erin Y Kim <erinykim04@gmail.com>
2024-02-05 19:33:30 -0800

    Adding 404 page view

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.render('Home');
+    res.render('error404');
 });
 
 app.set('view engine', 'jsx')
