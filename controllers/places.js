364a1079f6d204e4d15acc67dfc54f74667411c8
Erin Y Kim <erinykim04@gmail.com>
2024-02-07 21:45:24 -0800

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -12,8 +12,19 @@ router.get("/:id", (req, res) => {
   res.render("places/show", { place });
 });
 router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("POST /places");
+    console.log(req.body)
+  if (!req.body.pic) {
+    //Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.city) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 });
 
 module.exports = router;
