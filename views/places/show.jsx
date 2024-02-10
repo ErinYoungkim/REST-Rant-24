326d6103e69c44ccb5615e868072f02a0ec9ab83
Erin Y Kim <erinykim04@gmail.com>
2024-02-09 21:22:56 -0800

    route connections works officially

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,14 +1,23 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show ({place}) {
     return (
         <Def>
-          <main>
-            <h1>Show Page</h1>
+          <main className='container'>
+            <div className='row align-items-start'>
+                <div className='col'>
+                    <img src={place.pic} alt={place.name}/>
+                </div>
+                <div className='col'>
+                    <h1>{place.name}</h1>
+                    <p>{place.cuisines}</p>
+                </div>
+            </div>
           </main>
         </Def>
     )
 }
 
 module.exports = show
+
