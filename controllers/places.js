356b0c43e615b0c69a97502ca2f8e1d982ce113f
Erin Y Kim <erinykim04@gmail.com>
2024-02-09 23:08:18 -0800

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -8,32 +8,41 @@ router.get("/", (req, res) => {
 router.get("/new", (req, res) => {
   res.render("places/new");
 });
-router.get('/:id', (req, res) => {
-  const id = Number(req.params.id)
+router.get("/:id", (req, res) => {
+  const id = Number(req.params.id);
   if (isNaN(id)) {
-    res.status(404).render('error404')
-  }
-  else if (!places[id]) {
-    res.status(404).render('error404')
-  }
-  else {
-    res.render('places/show', { place: places[id] })
+    res.status(404).render("error404");
+  } else if (!places[id]) {
+    res.status(404).render("error404");
+  } else {
+    res.render("places/show", { place: places[id], id });
   }
 });
 router.post("/", (req, res) => {
-    console.log(req.body)
+  console.log(req.body);
   if (!req.body.pic) {
     //Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
+    req.body.pic = "http://placekitten.com/400/400";
   }
   if (!req.body.city) {
-    req.body.city = 'Anytown'
+    req.body.city = "Anytown";
   }
   if (!req.body.city) {
-    req.body.state = 'USA'
+    req.body.state = "USA";
   }
-  places.push(req.body)
-  res.redirect('/places')
+  places.push(req.body);
+  res.redirect("/places");
+});
+router.delete("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    places.splice(id, 1);
+    res.redirect("/places");
+  }
 });
 
 module.exports = router;
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,23 +1,39 @@
 const React = require("react");
 const Def = require("../default");
 
-function show({ place }) {
-  const cuisinesBadges = place.cuisines.split(",").map((cuisine) => {
-    return <span key = {cuisine} className="badge text-bg-info me-2">{cuisine}</span>;
+function show(data) {
+  const cuisinesBadges = data.place.cuisines.split(",").map((cuisine) => {
+    return (
+      <span key={cuisine} className="badge text-bg-info me-2">
+        {cuisine}
+      </span>
+    );
   });
   return (
     <Def>
       <main className="container">
         <div className="row align-items-start">
           <div className="col">
-            <img src={place.pic} alt={place.name} />
+            <img src={data.place.pic} alt={data.place.name} />
           </div>
           <div className="col">
-            <h1>{place.name}</h1>
+            <h1>{data.place.name}</h1>
             <p>
-              {place.city}, {place.state}
+              {data.place.city}, {data.place.state}
             </p>
             <p>{cuisinesBadges}</p>
+          </div>
+        </div>
+        <div className="row align-item-center">
+          <div className="col">
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning my-2">
+              Edit
+            </a>
+            <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+              <button type="submit" className="btn btn-danger my-2">
+                Delete
+              </button>
+            </form>
           </div>
         </div>
       </main>
