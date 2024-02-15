ce131dabcbc9d2f6003426beff062db0877a261a
Erin Y Kim <erinykim04@gmail.com>
2024-02-14 19:54:14 -0800

    created logic to node seeders to edit and delete

--- models/places.js
+++ models/places.js
@@ -21,7 +21,7 @@ const mongoose = require('mongoose')
 
 const placeSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  pic: { type: String, default: 'http//placekitten.com/350/350'},
+  pic: { type: String, default: 'http://placekitten.com/350/350'},
   cuisines: { type: String, required: true },
   city: { type: String, default: 'Anytown' },
   state: { type: String, default: 'USA' },
@@ -36,7 +36,4 @@ placeSchema.methods.showEstablished = function() {
 }
 
 module.exports = mongoose.model('Place', placeSchema)
-
-
-
 
--- seeders/seed-places.js
+++ seeders/seed-places.js
@@ -1,25 +1,36 @@
-const db = require('../models')
+const db = require("../models");
 
-db.Place.create([{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/h-thai-ml-tables.jpg',
-    founded: 1989
-}, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/coffee-cat.jpg',
-    founded: 2020
-}])
-.then(() => {
-    console.log('Success!')
-    process.exit()
-})
-.catch(err => {
-    console.log('Failure!', err)
-    process.exit()
-})
+db.Place.deleteMany({}).then(() => {
+  console.log("Exisiting places deleted successfully");
+
+  db.Place.create([
+    {
+      name: "H-Thai-ML",
+      city: "Seattle",
+      state: "WA",
+      cuisines: "Thai, Pan-Asian",
+      pic: "https://placekitten.com/350/350",
+      founded: 1989,
+    },
+    {
+      name: "Coding Cat Cafe",
+      city: "Phoenix",
+      state: "AZ",
+      cuisines: "Coffee, Bakery",
+      pic: "https://placekitten.com/350/350",
+      founded: 2020,
+    },
+  ])
+    .then(() => {
+      console.log("Success!");
+      process.exit();
+    })
+    .catch((err) => {
+      console.log("Failure!", err);
+      process.exit();
+    })
+    .catch((err) => {
+      console.log("Failure!", err);
+      process.exit(1);
+    });
+});
